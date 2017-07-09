6fd5b7c6330f8f19f7923f46affc01979896017c
Alexander Trauzzi <atrauzzi@gmail.com>
2017-07-08 20:05:22 -0500

    fix: Don't do this!

--- src/ReactServiceProvider.ts
+++ src/ReactServiceProvider.ts
@@ -8,25 +8,7 @@ export class ReactServiceProvider extends ServiceProvider {
 
     public async boot() {
 
-        this.bundle.container.bind(reduxSymbols.InitialState)
-            .to(this.findInitialState())
-            .inSingletonScope();
-
         this.bundle.container.rebind(reduxSymbols.Compose)
             .toConstantValue(window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose);
-    }
-
-    protected findInitialState() {
-
-        const element = document.getElementsByName("state")[0];
-
-        try {
-
-            return JSON.parse(element.nodeValue);
-        }
-        catch (error) {
-
-            return null;
-        }
     }
 }
